3b6e3018a3fc7ac26400eeb3ab23371000299423
marti <martines.607@gmail.com>
2023-04-20 08:24:50 -0500

    implement

---
+++ src/main/java/com/safra/InventaryEntryAndExitService/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.safra.InventaryEntryAndExitService.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI orchestratorServiceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Assembling Service API")
+                        .description("Endpoints and Models for Assembling Service Application")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+}
