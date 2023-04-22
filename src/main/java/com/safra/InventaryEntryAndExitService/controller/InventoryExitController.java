6ee101c81d37d1d29876cd67047fbf948a5ce737
marti <martines.607@gmail.com>
2023-04-22 13:12:49 -0500

    implement

--- src/main/java/com/safra/InventaryEntryAndExitService/controller/InventoryExitController.java
+++ src/main/java/com/safra/InventaryEntryAndExitService/controller/InventoryExitController.java
@@ -55,19 +55,4 @@ public class InventoryExitController {
 		return null;
 	}
 
-	@PostMapping("test")
-	public List<OIBT> test(HttpServletRequest request) {
-		HttpSession session = request.getSession();
-		new CertificateSSLConfig().trustSelfSignedSSL();
-		try {
-			return inventoryExitService.ExitRollsStockMin((HttpHeaders) session.getAttribute("cookies"));
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-		return null;
-	}
-
-
-
-
 }
